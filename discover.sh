d1876fb4d0273f8a785d19de921ec7e5e87f12de
Leo Zingaya <19838800+zingaya@users.noreply.github.com>
2024-07-24 12:42:14 +0100

    Create discover.sh
    Additional script to discover hosts with SSH port open.

---
+++ discover.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Define the subnets
+subnets=$1
+
+# List of IPs to exclude
+excluded_ips=""
+
+# File to store the reachable hosts
+ansible_hosts_file="/root/sysadmin/playbooks/discovered-hosts"
+
+# Initialize the Ansible hosts file
+echo "[all]" > $ansible_hosts_file
+
+# Function to check if SSH is open
+check_ssh() {
+  ip=$1
+  sh -c "timeout 0.5 echo -n | nc $ip 22 > /dev/null" && echo $ip >> $ansible_hosts_file
+}
+
+# Loop through the subnets and IPs
+for subnet in $subnets; do
+  for i in $(seq 1 254); do
+    ip="$subnet.$i"
+    # Check if the IP is in the excluded list
+    if echo "$excluded_ips" | grep -qw "$ip"; then
+      continue
+    fi
+    check_ssh $ip &
+  done
+  wait
+done
+
+# Wait for all background processes to complete
+wait
+
+echo "Ansible hosts file created: $ansible_hosts_file"
